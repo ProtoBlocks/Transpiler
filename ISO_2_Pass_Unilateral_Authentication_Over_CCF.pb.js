b80404a7232ea09eeb2ff821e75c0f9a5f4f4da6
Vivek Nair <vivek@nair.me>
2021-11-01 07:15:23 -0700

    Update ISO_2_Pass_Unilateral_Authentication_Over_CCF.pb.js

--- ISO_2_Pass_Unilateral_Authentication_Over_CCF.pb.js
+++ ISO_2_Pass_Unilateral_Authentication_Over_CCF.pb.js
@@ -7,8 +7,13 @@ protocol ISO_2_Pass_Unilateral_Authentication_Over_CCF [Prover(Secret), Verifier
   }
 
   step Response [Prover -> Verifier] {
-    const Hash = hash(Challenge.Nonce + Verifier.Id + Prover.Input.Secret);
+    const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Prover.Input.Secret);
     Verifier.send({"Hash": Hash});
+  }
+  
+  step Verify [Verifier] {
+    const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Verifier.Input.Secret);
+    return (Hash === Prover.Response.Hash);
   }
 }
 
